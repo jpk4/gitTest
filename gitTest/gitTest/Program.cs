1a6b2f52571e80b77f3a9cec36ef5f6c3c1653f7
jpk4 <jessicakleysen@gmail.com>
2019-09-06 00:16:33 -0500

    Initial Commit

---
+++ gitTest/gitTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my initial commit!");
+            Console.ReadKey();
+        }
+    }
+}
